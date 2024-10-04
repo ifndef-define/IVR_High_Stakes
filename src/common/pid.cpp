eb559191bb1593d472fca80b62e07b4782c2aa2f
Joshua Chen <jhc3628@gmail.com>
2024-10-04 00:06:54 -0500

    Dancing Robot, but integral term is broken.

--- src/common/pid.cpp
+++ src/common/pid.cpp
@@ -1,5 +1,6 @@
 #include "common/pid.h"
 #include "pros/rtos.hpp"
+#include "main.h"
 
 void PID::setCoefficients(double p, double i, double d) {
     this->p = p;
@@ -8,13 +9,17 @@ void PID::setCoefficients(double p, double i, double d) {
 }
 
 double PID::update(double error) {
+    double value = 0;
     if (lastTime >= 0) {
-        double dt = pros::millis()/1e3 - lastTime;
+        double dt = pros::micros()/1e6 - lastTime;
         double de_dt = (error - lastError) / dt;
         accumulator += error * dt;
 
-        return p * error + i * accumulator + d * de_dt;
+        value = p * error + d * de_dt;
+        pros::lcd::print(1, "p %f, i %f, d %f, a %f, %f de_dt", p, i, d, accumulator, de_dt);
+        pros::lcd::print(2, "a %f, %f de_dt, %f dt", accumulator, de_dt, dt);
     }
     lastError = error;
-    lastTime = pros::millis()/1e3;
+    lastTime = pros::micros()/1e6;
+    return value;
 };
--- src/comp-15/main.cpp
+++ src/comp-15/main.cpp
@@ -1,4 +1,7 @@
 #include "main.h"
+#include "common/pid.h"
+#include "pros/imu.hpp"
+#include "pros/rotation.hpp"
 #include "robots/comp-15/auton.h"
 #include "robots/comp-15/controls.h"
 
@@ -18,4 +21,26 @@ void competition_initialize() {}
 void autonomous() {}
 
 /* Driver Control. Runs default if not connected to field controler */
-void opcontrol() {}
+void opcontrol() {
+	PID pid1(2, 0, 0.5);
+	PID pid2(2, 0, 0.5);
+	pros::Motor motor1(16, MotorGears::green, MotorEncoderUnits::degrees);
+	pros::Motor motor2(12, MotorGears::green, MotorEncoderUnits::degrees);
+	pros::IMU imu(14);
+
+	double angle = 180;
+
+	while (true) {
+		double error = angle - imu.get_rotation();
+		double error1 = pid1.update(error);
+		double error2 = pid2.update(error);
+		motor1.move(error1);
+		motor2.move(error2);
+
+		if (std::abs(error) < 5) {
+			angle = - angle;
+		}
+
+		delay(100);
+	}
+}
