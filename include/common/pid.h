c5497a9dbead0b62544afa0c5cf9f71f441e91e9
Carter Johns <cartermjohns@outlook.com>
2024-10-08 21:03:29 -0500

    Co-authored-by: Rishi V. <v3rac1ty@users.noreply.github.com> Co-authored-by: Anissh G <ifndef-define@users.noreply.github.com> Co-authored-by: MGray24 <MGray24@users.noreply.github.com> Co-authored-by: Eric <ezh2@users.noreply.github.com>

--- include/common/pid.h
+++ include/common/pid.h
@@ -2,14 +2,57 @@
 #include "main.h"
 
 class PID {
-private:
-  double p, i, d;
-  double accumulator;
-  double lastError;
-  double lastTime;
+    private:
+        // patch for hole
+        bool firstPIDrun = true;
+        // All private variables like PID constants, sensor and/or motor pointers
+        struct PID_consts_s{
+            double kP = 0;
+            double kI = 0;
+            double kD = 0;
+            double outMax = 0;
+        };
+        PID_consts_s pid_consts_;
 
-public:
-  PID(double p = 0, double i = 0, double d = 0) : p{p}, i{i}, d{d}, accumulator{0}, lastError{0}, lastTime{-1} {}
-  void setCoefficients(double p, double i, double d);
-  double update(double error);
+        // PID state variables like error, lastError, derivative, integral, etc
+        // This struct can be used to store the state of the PID loop so if 
+        // edge cases like saturation, etc can be handled by the user
+        struct PID_state_s{
+            double error = 0;
+            double lastError = 0;
+            double derivative = 0;
+            double integral = 0;
+            double rawOut = 0;
+            bool saturated = false;
+        };
+        PID_state_s pid_state_;
+
+    public:
+        PID (double kP, double kI, double kD, double outMax);
+        PID (PID_consts_s inpConsts);
+		PID (PID &other); // Copy constructor
+
+        // Assignment operator overload for copying PID objects
+        PID &operator=(PID & other);
+
+		// A simple runPID method that takes in the target and current value
+        double runPID(double target, double current);
+        void setConstants(double kP, double kI, double kD, double outMax);
+
+        // Getters and Setters for PID constants
+        PID_consts_s getConstants();
+        PID_state_s getState();
 };
+
+// class PID {
+// private:
+//   double p, i, d;
+//   double accumulator;
+//   double lastError;
+//   double lastTime;
+
+// public:
+//   PID(double p = 0, double i = 0, double d = 0) : p{p}, i{i}, d{d}, accumulator{0}, lastError{0}, lastTime{-1} {}
+//   void setCoefficients(double p, double i, double d);
+//   double update(double error);
+// };
--- src/common/pid.cpp
+++ src/common/pid.cpp
@@ -1,25 +1,99 @@
 #include "common/pid.h"
-#include <cmath>
 
-void PID::setCoefficients(double p, double i, double d) {
-    this->p = p;
-    this->i = i;
-    this->d = d;
+PID::PID (double kP, double kI, double kD, double outMax) {
+    // setting constants seperatly with constructor
+    pid_consts_.kP = kP; //proportion
+    pid_consts_.kI = kI; //integral
+    pid_consts_.kD = kD; //derivative
+    pid_consts_.outMax = outMax; //used to determine saturation
+}
+PID::PID (PID_consts_s inpConsts){
+    // setting constants the lazy way
+    pid_consts_ = inpConsts;
+};
+PID::PID (PID &other){
+    pid_consts_ = other.pid_consts_;
+}; // Copy constructor
+
+// Assignment operator overload for copying PID objects
+PID &PID::operator=(PID & other) {
+    // Deep copy of parameters
+    this->pid_consts_ = other.pid_consts_;
+    this->pid_state_ = other.pid_state_;
+    return *this;
+};
+
+void PID::setConstants(double kP, double kI, double kD, double outMax) {
+    //setting constants
+    pid_consts_.kP = kP;
+    pid_consts_.kI = kI;
+    pid_consts_.kD = kD;
+    pid_consts_.outMax = outMax;
 }
 
-double PID::update(double error) {
-    double value = 0;
-    if (lastTime >= 0) {
-        double dt = pros::micros()/1e6 - lastTime;
-        double de_dt = (error - lastError) / dt;
-        accumulator += error * dt;
-        if (std::fabs(accumulator) == infinity()) {
-            accumulator = 0;
-        }
+/**
+ * @brief About the PID calculation method
+ * 
+ * What to do per PID loop:
+ * - Calculate the error ✔️
+ * - Calculate the integral ✔️
+ *   - Check for integral windup               
+ * - Calculate the derivative ✔️ 
+ * - Calculate the output ✔️ 
+ *   - Determine if saturated
+ * - Update the last error ✔️ 
+ */
+double PID::runPID(double target, double current) {
+    
+    
+    pid_state_.error = target - current;  //Error is the target value minus the current value
+    pid_state_.derivative = pid_state_.error - pid_state_.lastError;  //Get derivative by subtacting current error and last error
+    pid_state_.integral = pid_state_.integral + pid_state_.error;  //Add current error to integral
+    
 
-        value = p * error + i * accumulator + d * de_dt;
+    double p = pid_state_.error * pid_consts_.kP;
+    double i = 0; //pid_state.integral * pid_consts_.kI; // delete "0; //" to enable integral term
+    double d = pid_state_.derivative * pid_consts_.kD;
+
+    if(firstPIDrun){    
+        d = 0;
+        firstPIDrun = false;
     }
-    lastError = error;
-    lastTime = pros::micros()/1e6;
-    return value;
-};
+    if (std::fabs(pid_state_.rawOut) >  pid_consts_.outMax){
+        // checking for saturation
+        pid_state_.saturated = true;
+    }
+    pid_state_.lastError = pid_state_.error;  //Set last error to what the error currently is
+    return p + i + d;
+    
+}
+
+
+
+// void skiPIDi::ohio(fanum tax) {
+//     rizzler.getLivvy = BABYgronk
+//     betaMale.getBeats = ksi;
+//     alphaMale.getLunch = lunchly;
+// }
+// void PID::setCoefficients(double p, double i, double d) {
+//     this->p = p;
+//     this->i = i;
+//     this->d = d;
+// }
+
+// double PID::update(double error) {
+//     double value = 0;
+//     if (lastTime >= 0) {
+//         double dt = pros::micros()/1e6 - lastTime;
+//         double de_dt = (error - lastError) / dt;
+//         accumulator += error * dt;
+//         if (std::abs(accumulator) == infinity()) {
+//             accumulator = 0;
+//         }
+
+//         value = p * error + i * accumulator + d * de_dt;
+//     }
+//     lastError = error;
+//     lastTime = pros::micros()/1e6;
+//     return value;
+// };
