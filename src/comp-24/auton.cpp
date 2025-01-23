eb14e09cee53d3574ab8e962a04545ef05282b99
ryanbendersky <rhb4@illinois.edu>
2025-01-23 00:39:29 -0600

    comp autos

--- src/comp-24/auton.cpp
+++ src/comp-24/auton.cpp
@@ -50,56 +50,35 @@ ASSET(comp_white_7_txt);
 ASSET(comp_white_8_txt);
 ASSET(comp_white_9_txt);
 void redAuton(){
-    chassis.setPose(-53.5, 61, 90);
+    chassis.setPose(-40, 63, 90);
     uint32_t timeStart = pros::millis();
     intake.setAutonControlFlag(false);
     intakeMotor.move(0);
-    intakeLift.extend();
+    // intakeLift.extend();
     mogoRushReach.extend();
     mogoRushClamp.extend(); //reversed
-    chassis.follow(comp_white_1_txt, 15, 1500, 1, 0);
-
+    chassis.moveToPose(-11, 60, 102, 1700, {.maxSpeed = 127,
+                                            .minSpeed = 100}, 0);
+    i_waitUntil(!chassis.isInMotion());
+    delay(250);
     mogoRushClamp.retract();
-    chassis.moveToPose(-44, 60, 90, 1700, {.forwards = false,
+    chassis.moveToPose(-33, 60, 90, 1700, {.forwards = false,
                                             .maxSpeed = 127,
-                                            .minSpeed = 127}, 0);
+                                            .minSpeed = 80}, true);
 
-    chassis.tank(0, 0, 0);
-    delay(500);
-
+    delay(600);
+    mogoRushClamp.extend();
+    mogoRushReach.retract();
+    i_waitUntil(!chassis.isInMotion());
     chassis.turnToHeading(270, 1400,{.direction = lemlib::AngularDirection::CCW_COUNTERCLOCKWISE,
                                     .maxSpeed = 127/2});
-    while(chassis.isInMotion()){
-        if(chassis.getPose().theta <= 80){
-            mogoRushClamp.extend();
-            mogoRushReach.retract();
-        }
-        delay(15);
-    }
-    delay(250);
 
-    chassis.follow(comp_white_2_txt, 15, 5000, 0);
-    while(chassis.isInMotion()){
-        if(chassis.getPose().y <= 50){
-            mogoClamp.extend();
-        }
-        delay(15);
-    }
+    // chassis.follow(comp_white_2_txt, 15, 5000, 0);
+    chassis.moveToPose(-5, 60, 275, 1000, {.forwards = false}, 0);
+    mogoClamp.extend();
+    chassis.turnToHeading(225, 1400,{.maxSpeed = 127/2});
 
-    chassis.moveToPose(-22, 36, 345, 3000, {.maxSpeed = 127/2}, 1);
-    while(chassis.isInMotion()){
-        if(chassis.getPose().y >= 28){
-            intake.setAutonControlFlag(true);
-            intakeMotor.move(127);
-        }
-        delay(15);
-    }
-
-    intakeLift.retract();
-    delay(400);
-    chassis.swingToHeading(315, lemlib::DriveSide::RIGHT, 800, {}, 0);
     intake.setAutonControlFlag(true);
-    intakeMotor.move(127);
     // chassis.moveToPose(-46, 48, 270, 3000, {.maxSpeed = 127/4}, 0);
     // chassis.moveToPose(-69, 69, 315, 1500, {}, 0);
     chassis.follow(comp_white_4_txt, 15, 3000, 1, 1);
