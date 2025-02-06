cf77d3f3ad43174c7e3871ab30718ccb07b8e77c
Rishi V. <rishivemulapalli@gmail.com>
2025-02-05 18:08:45 -0600

    more readable

--- src/comp-15/arm.cpp
+++ src/comp-15/arm.cpp
@@ -1,7 +1,7 @@
 #include "robots/comp-15/arm.h"
 
 void Arm::update(){
-    
+
 }
 
 void Arm::setState(armState_t newState) { armState = newState; }
@@ -10,10 +10,6 @@ void Arm::nextState(){ armState = (armState_t)((armState + 1) % NUM_STATES); }
 
 void Arm::prevState(){ armState = (armState_t)((armState + NUM_STATES - 1) % NUM_STATES); }
 
-double Arm::getAngle(){
-    return armRot.get_position()*100;
-}
+double Arm::getAngle(){ return armRot.get_position()*100; }
 
-Arm::armState_t Arm::getState(){
-    return armState;
-}
+Arm::armState_t Arm::getState(){ return armState; }
