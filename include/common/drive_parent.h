ba96ccacff27d3bd49a86fb3967dd1842ad3e5e2
ifndef-define <66566910+ifndef-define@users.noreply.github.com>
2025-04-18 16:55:33 -0500

    moved files

--- include/common/drive_parent.h
+++ include/common/drive_parent.h
@@ -51,108 +51,6 @@ class drive {
         static void turnAtRPM(int rpm);
 
         /**
-         * @brief Moves the robot using a vector field. This method requires a polynomial function to be
-         *  calculated and its x and y equations passed in as a funciton to determine the vector to move the robot.
-         * 
-         * @param x_comp X component equation of the vector field function
-         * @param y_comp Y component equation of the vector field function
-         * @param heading Angle to end at [0,360] degrees
-         * @param min_speed Minimum speed to move at [0,1] percent of max speed
-         * @param max_speed Maximum speed to move at [0,1] percent of max speed
-         *  NOTE: max_speed > min_speed
-         * @param timeout Time to wait for the move to complete in milliseconds
-         * @param wait If true, will wait for the move to complete before returning
-         *  NOTE: Queuing is supported with mutex locks
-         * 
-         * @example
-         * \code{.cpp}
-         * // Move via the equation y = x^3
-         */
-        static void move_VF(std::function<double(double, double, double)> x_comp, std::function<double(double, double, double)> y_comp,
-            double gain, double func_degree, double heading, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
-
-        /**
-         * @brief Moves the robot to a set pose (x, y, theta). Field (0,0) is the red alliance
-         *  positive corner.
-         * 
-         * @param x X coordinate to move to [0,144] inches
-         * @param y Y coordinate to move to [0,144] inches
-         * @param theta Angle to move to [0,360] degrees
-         * @param min_speed Minimum speed to move at [0,1] percent of max speed
-         * @param max_speed Maximum speed to move at [0,1] percent of max speed
-         *  NOTE: max_speed > min_speed
-         * @param timeout Time to wait for the move to complete in milliseconds
-         * @param wait If true, will wait for the move to complete before returning
-         *  NOTE: Queuing is supported with mutex locks
-         */
-        static void moveToPose(double x, double y, double theta, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
-
-        /**
-         * @brief Turns the robot to face a set pose. Field (0,0) is the red alliance
-         *  positive corner.
-         * 
-         * @param x X coordinate to face [0,144] inches
-         * @param y Y coordinate to face [0,144] inches
-         * @param min_speed Minimum speed to move at [0,1] percent of max speed
-         * @param max_speed Maximum speed to move at [0,1] percent of max speed
-         *  NOTE: max_speed > min_speed
-         * @param timeout Time to wait for the move to complete in milliseconds
-         * @param wait If true, will wait for the move to complete before returning
-         *  NOTE: Queuing is supported with mutex locks
-         */
-        static void turnToPose(double x, double y, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
-
-        /**
-         * @brief Turns the robot to face a set heading. Field (0,0) is the red alliance
-         *  positive corner.
-         * 
-         * @param theta Angle to face [0,360] degrees
-         * @param min_speed Minimum speed to move at [0,1] percent of max speed
-         * @param max_speed Maximum speed to move at [0,1] percent of max speed
-         *  NOTE: max_speed > min_speed
-         * @param timeout Time to wait for the move to complete in milliseconds
-         * @param wait If true, will wait for the move to complete before returning
-         *  NOTE: Queuing is supported with mutex locks  
-         */
-        static void turnToHeading(double theta, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
-        
-        enum drive_side_e {
-            LEFT = 0,
-            RIGHT = 1
-        };
-
-        /**
-         * @brief Moves the robot to face a set pose (x, y) while keeping one side of the drive
-         *  locked in place. Field (0,0) is the red alliance positive corner.
-         * 
-         * @param x X coordinate to move to [0,144] inches
-         * @param y Y coordinate to move to [0,144] inches
-         * @param locked_side Side to lock in place
-         * @param min_speed Minimum speed to move at [0,1] percent of max speed
-         * @param max_speed Maximum speed to move at [0,1] percent of max speed
-         *  NOTE: max_speed > min_speed
-         * @param timeout Time to wait for the move to complete in milliseconds
-         * @param wait If true, will wait for the move to complete before returning
-         *  NOTE: Queuing is supported with mutex locks
-         */
-        static void swingToPose(double x, double y, drive_side_e locked_side, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
-
-        /**
-         * @brief Moves the robot to a set heading while keeping one side of the drive
-         *  locked in place. Field (0,0) is the red alliance positive corner.  
-         * 
-         * @param theta Angle to face [0,360] degrees
-         * @param locked_side Side to lock in place
-         * @param min_speed Minimum speed to move at [0,1] percent of max speed
-         * @param max_speed Maximum speed to move at [0,1] percent of max speed
-         *  NOTE: max_speed > min_speed
-         * @param timeout Time to wait for the move to complete in milliseconds
-         * @param wait If true, will wait for the move to complete before returning
-         *  NOTE: Queuing is supported with mutex locks 
-         */
-        static void swingToHeading(double theta, drive_side_e locked_side, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
-
-        /**
          * @brief Determines values to move the robot given a vector. This will compute 
          *  the vector from the current position to the target position and move the robot
          * 
@@ -160,12 +58,6 @@ class drive {
          * @param y Y component of the vector [0,144] inches
          */
         static void moveByVector(double x, double y, double min_speed, double max_speed);
-
-        /**
-         * @brief Moves the robot given a normalized vector. This will compute the vector from the 
-         *  current position to the target position and move the robot
-         */
-        static void moveByVector_normalized(double x, double y, double min_speed, double max_speed);
 
         /**
          * @brief Moves the robot given a normalized left and right drive power value.
---
+++ include/common/motion.h
@@ -0,0 +1,106 @@
+#pragma once
+#include "main.h"
+
+/**
+         * @brief Moves the robot using a vector field. This method requires a polynomial function to be
+         *  calculated and its x and y equations passed in as a funciton to determine the vector to move the robot.
+         * 
+         * @param x_comp X component equation of the vector field function
+         * @param y_comp Y component equation of the vector field function
+         * @param heading Angle to end at [0,360] degrees
+         * @param min_speed Minimum speed to move at [0,1] percent of max speed
+         * @param max_speed Maximum speed to move at [0,1] percent of max speed
+         *  NOTE: max_speed > min_speed
+         * @param timeout Time to wait for the move to complete in milliseconds
+         * @param wait If true, will wait for the move to complete before returning
+         *  NOTE: Queuing is supported with mutex locks
+         * 
+         * @example
+         * \code{.cpp}
+         * // Move via the equation y = x^3
+         * double x_comp(double x, double y, double k) {
+         *  return 1 + 3*k*pow(x, 2) * (y - pow(x,3));
+         */
+        static void move_VF(std::function<double(double, double, double)> x_comp, std::function<double(double, double, double)> y_comp,
+            double gain, double func_degree, double heading, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
+
+        /**
+         * @brief Moves the robot to a set pose (x, y, theta). Field (0,0) is the red alliance
+         *  positive corner.
+         * 
+         * @param x X coordinate to move to [0,144] inches
+         * @param y Y coordinate to move to [0,144] inches
+         * @param theta Angle to move to [0,360] degrees
+         * @param min_speed Minimum speed to move at [0,1] percent of max speed
+         * @param max_speed Maximum speed to move at [0,1] percent of max speed
+         *  NOTE: max_speed > min_speed
+         * @param timeout Time to wait for the move to complete in milliseconds
+         * @param wait If true, will wait for the move to complete before returning
+         *  NOTE: Queuing is supported with mutex locks
+         */
+        static void moveToPose(double x, double y, double theta, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
+
+        /**
+         * @brief Turns the robot to face a set pose. Field (0,0) is the red alliance
+         *  positive corner.
+         * 
+         * @param x X coordinate to face [0,144] inches
+         * @param y Y coordinate to face [0,144] inches
+         * @param min_speed Minimum speed to move at [0,1] percent of max speed
+         * @param max_speed Maximum speed to move at [0,1] percent of max speed
+         *  NOTE: max_speed > min_speed
+         * @param timeout Time to wait for the move to complete in milliseconds
+         * @param wait If true, will wait for the move to complete before returning
+         *  NOTE: Queuing is supported with mutex locks
+         */
+        static void turnToPose(double x, double y, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
+
+        /**
+         * @brief Turns the robot to face a set heading. Field (0,0) is the red alliance
+         *  positive corner.
+         * 
+         * @param theta Angle to face [0,360] degrees
+         * @param min_speed Minimum speed to move at [0,1] percent of max speed
+         * @param max_speed Maximum speed to move at [0,1] percent of max speed
+         *  NOTE: max_speed > min_speed
+         * @param timeout Time to wait for the move to complete in milliseconds
+         * @param wait If true, will wait for the move to complete before returning
+         *  NOTE: Queuing is supported with mutex locks  
+         */
+        static void turnToHeading(double theta, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
+        
+        enum drive_side_e {
+            LEFT = 0,
+            RIGHT = 1
+        };
+
+        /**
+         * @brief Moves the robot to face a set pose (x, y) while keeping one side of the drive
+         *  locked in place. Field (0,0) is the red alliance positive corner.
+         * 
+         * @param x X coordinate to move to [0,144] inches
+         * @param y Y coordinate to move to [0,144] inches
+         * @param locked_side Side to lock in place
+         * @param min_speed Minimum speed to move at [0,1] percent of max speed
+         * @param max_speed Maximum speed to move at [0,1] percent of max speed
+         *  NOTE: max_speed > min_speed
+         * @param timeout Time to wait for the move to complete in milliseconds
+         * @param wait If true, will wait for the move to complete before returning
+         *  NOTE: Queuing is supported with mutex locks
+         */
+        static void swingToPose(double x, double y, drive_side_e locked_side, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
+
+        /**
+         * @brief Moves the robot to a set heading while keeping one side of the drive
+         *  locked in place. Field (0,0) is the red alliance positive corner.  
+         * 
+         * @param theta Angle to face [0,360] degrees
+         * @param locked_side Side to lock in place
+         * @param min_speed Minimum speed to move at [0,1] percent of max speed
+         * @param max_speed Maximum speed to move at [0,1] percent of max speed
+         *  NOTE: max_speed > min_speed
+         * @param timeout Time to wait for the move to complete in milliseconds
+         * @param wait If true, will wait for the move to complete before returning
+         *  NOTE: Queuing is supported with mutex locks 
+         */
+        static void swingToHeading(double theta, drive_side_e locked_side, double min_speed=0.1, double max_speed=0.1, int timeout=10000, bool wait=true);
